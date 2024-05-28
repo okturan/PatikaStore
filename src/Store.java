f2b85e80d5b652b5499c60cc08c71508661f4457
okturan <okanert@gmail.com>
2024-05-28 08:54:13 +0300

    terminal output fixes

--- src/Store.java
+++ src/Store.java
@@ -10,6 +10,9 @@ public class Store {
 	ArrayList<Product> phones;
 	Brand[] brands;
 
+	public static int notebookCounter = 0;
+	public static int phoneCounter = 0;
+
 	public void stockInventory () {
 		String[] brandNames = {"Samsung", "Lenovo", "Apple", "Huawei", "Casper", "Asus", "HP", "Xiaomi", "Monster"};
 		Arrays.sort(brandNames);
@@ -21,16 +24,20 @@ public class Store {
 		}
 
 		notebooks = new ArrayList<>();
-		notebooks.add(new Notebook(1, 999.99, 10.0, 50, "MacBook Pro", brands[0], 512, 16, 16));
-		notebooks.add(new Notebook(2, 799.99, 15.0, 30, "Dell XPS 13", brands[5], 256, 13, 8));
-		notebooks.add(new Notebook(3, 649.99, 5.0, 40, "HP Spectre x360", brands[3], 512, 14, 8));
+		notebooks.add(new Notebook(++notebookCounter, 999.99, 10.0, 50, "MacBook Pro", brands[0], 512, 16, 16));
+		System.out.println(notebookCounter);
+		notebooks.add(new Notebook(++notebookCounter, 799.99, 15.0, 30, "Dell XPS 13", brands[5], 256, 13, 8));
+		System.out.println(notebookCounter);
+		notebooks.add(new Notebook(++notebookCounter, 649.99, 5.0, 40, "HP Spectre x360", brands[3], 512, 14, 8));
+		System.out.println(notebookCounter);
 
 		phones = new ArrayList<>();
-		phones.add(new Phone(1, 999.99, 20.0, 100, "iPhone 13 Pro", brands[0], 256, 6, 6, 3095, "Graphite"));
-		phones.add(new Phone(2, 799.99, 25.0, 80, "Samsung Galaxy S21", brands[7], 128, 6, 8, 4000, "Phantom Gray"));
-		phones.add(new Phone(3, 699.99, 10.0, 60, "Xiaomi note 9", brands[8], 128, 6, 8, 4614, "Stormy Black"));
+		phones.add(new Phone(++phoneCounter, 999.99, 20.0, 100, "iPhone 13 Pro", brands[0], 256, 6, 6, 3095, "Graphite"));
+		phones.add(new Phone(++phoneCounter, 799.99, 25.0, 80, "Samsung Galaxy S21", brands[7], 128, 6, 8, 4000, "Phantom Gray"));
+		phones.add(new Phone(++phoneCounter, 699.99, 10.0, 60, "Xiaomi Note 9", brands[8], 128, 6, 8, 4614, "Stormy Black"));
 
 		printNotebooks();
+		System.out.println(notebookCounter);
 		printPhones();
 	}
 
@@ -108,62 +115,60 @@ public class Store {
 	}
 
 	public void addNotebook () {
-		System.out.print("Enter Notebook ID: ");
-		int id = input.nextInt();
-		System.out.print("Enter Notebook price: ");
+		for (Brand brand : brands) {
+			System.out.println(brand.getId() + " - " + brand.getName());
+		}
+		System.out.print("Enter brand ID: ");
+		int brandId = input.nextInt();
+		input.nextLine(); // consume newline
+		System.out.print("Enter Notebook name: ");
+		String name = input.nextLine();
+		System.out.print("Enter Notebook price (USD): ");
 		double price = input.nextDouble();
 		System.out.print("Enter discount rate: ");
 		double discountRate = input.nextDouble();
 		System.out.print("Enter stock amount: ");
 		int stockAmount = input.nextInt();
-		input.nextLine(); // consume newline
-		System.out.print("Enter Notebook name: ");
-		String name = input.nextLine();
-		System.out.println("Enter brand ID: ");
-		for (Brand brand : brands) {
-			System.out.println(brand.getId() + " - " + brand.getName());
-		}
-		int brandId = input.nextInt();
-		System.out.print("Enter storage: ");
+		System.out.print("Enter storage (GB): ");
 		int storage = input.nextInt();
-		System.out.print("Enter screen size: ");
+		System.out.print("Enter screen size (inches): ");
 		int screenSize = input.nextInt();
-		System.out.print("Enter RAM: ");
+		System.out.print("Enter RAM (GB): ");
 		int ram = input.nextInt();
 
-		Product newProduct = new Notebook(id, price, discountRate, stockAmount, name, brands[brandId - 1], storage, screenSize, ram);
+		Product newProduct = new Notebook(++notebookCounter, price, discountRate, stockAmount, name, brands[brandId - 1], storage, screenSize, ram);
 		notebooks.add(newProduct);
 		System.out.println(newProduct + " added successfully.");
 	}
 
 	public void addPhone () {
-		System.out.print("Enter Phone ID: ");
-		int id = input.nextInt();
-		System.out.print("Enter Phone price: ");
+		for (Brand brand : brands) {
+			System.out.println(brand.getId() + " - " + brand.getName());
+		}
+		System.out.print("Enter brand ID: ");
+		int brandId = input.nextInt();
+		input.nextLine(); // consume newline
+		System.out.print("Enter Phone name: ");
+		String name = input.nextLine();
+		System.out.print("Enter Phone price (USD): ");
 		double price = input.nextDouble();
 		System.out.print("Enter discount rate: ");
 		double discountRate = input.nextDouble();
 		System.out.print("Enter stock amount: ");
 		int stockAmount = input.nextInt();
-		System.out.print("Enter Phone name: ");
-		String name = input.nextLine();
-		System.out.println("Enter brand ID: ");
-		for (Brand brand : brands) {
-			System.out.println(brand.getId() + " - " + brand.getName());
-		}
-		int brandId = input.nextInt();
-		System.out.print("Enter storage: ");
+		System.out.print("Enter storage (GB): ");
 		int storage = input.nextInt();
-		System.out.print("Enter screen size: ");
+		System.out.print("Enter screen size (inches): ");
 		int screenSize = input.nextInt();
-		System.out.print("Enter RAM: ");
+		System.out.print("Enter RAM (GB): ");
 		int ram = input.nextInt();
-		System.out.println("Enter battery capacity in Mah: ");
+		System.out.print("Enter battery capacity (Mah): ");
 		int batteryCapacity = input.nextInt();
-		System.out.print("Enter phone color");
+		input.nextLine(); // consume newline
+		System.out.print("Enter phone color: ");
 		String color = input.nextLine();
 
-		Product newProduct = new Phone(id, price, discountRate, stockAmount, name, brands[brandId - 1], storage, screenSize, ram, batteryCapacity, color);
+		Product newProduct = new Phone(++phoneCounter, price, discountRate, stockAmount, name, brands[brandId - 1], storage, screenSize, ram, batteryCapacity, color);
 		phones.add(newProduct);
 		System.out.println(newProduct + " added successfully.");
 	}
@@ -199,11 +204,21 @@ public class Store {
 	}
 
 	public void printNotebooks () {
+		System.out.println("\nNotebooks");
+		System.out.println("------------------------------------------------------------------------------------------------------");
+		System.out.printf("| %2s | %-8s | %-18s | %-10s | %-6s | %-3s | %-7s | %-10s | %-5s |\n", "ID", "Brand", "Name", "Price", "Discount", "Stock", "Storage", "Screen Size", "RAM");
+		System.out.println("------------------------------------------------------------------------------------------------------");
 		notebooks.forEach(System.out::println);
+		System.out.println("------------------------------------------------------------------------------------------------------");
 	}
 
 	public void printPhones () {
+		System.out.println("\nPhones");
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+		System.out.printf("| %2s | %-8s | %-18s | %-10s | %-6s | %-3s | %-7s | %-10s | %-5s | %-9s | %-12s |\n", "ID", "Brand", "Name", "Price", "Discount", "Stock", "Storage", "Screen Size", "RAM", "Battery", "Color");
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
 		phones.forEach(System.out::println);
+		System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
 	}
 
 	public void printBrands () {
